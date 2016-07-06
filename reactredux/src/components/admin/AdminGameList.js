b11894253e2a87e3af39f38b8df3262b2253b5e1
Ryan Michalec <rmichalec14@gmail.com>
2016-07-06 14:27:53 -0400

    some cleanup

---
+++ reactredux/src/components/admin/AdminGameList.js
@@ -0,0 +1,107 @@
+import React, {PropTypes} from 'react';
+import ConfirmGameListRow from './ConfirmGameListRow';
+
+// Need to come back and add columns/formatting
+
+const ConfirmGameList = ({games, users, uid, league, uid2, uid3, uid4, uids, currentUser, confirm, deny}) => {
+  
+  if (uid){
+    games = games.filter(function(game){
+      for (var i in game.player_names){
+        if( game.player_names[i] === uid){
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+  if(uid2){
+    games = games.filter(function(game){
+      for (var i in game.player_names){
+        if( game.player_names[i] === uid2){
+          return true;
+        }
+      }
+
+      return false;
+    })
+  }
+  if (uid3){
+    games = games.filter(function(game){
+      for (var i in game.player_names){
+        if( game.player_names[i] === uid3){
+          return true;
+        }
+      }
+
+      return false;
+    });
+  }
+  if(uid4){
+    games = games.filter(function(game){
+      for (var i in game.player_names){
+        if( game.player_names[i] === uid4){
+          return true;
+        }
+      }
+
+      return false;
+    })
+  }
+  for ( var j in uids){
+    games = games.filter(function(game){
+      for (var i in game.player_names){
+        if( game.player_names[i] === uids[j]){
+          return true;
+        }
+      }
+
+      return false;
+    })
+  }
+  if (league) {
+    games = games.filter(function(game){
+
+      if (game.league_name === league){
+        return true;
+      }
+      return false;
+    })
+  }
+
+  return (
+    <table className="table">
+      <thead>
+      <tr>
+        <th>Game Date</th>
+        <th>Left Team</th>
+        <th>Left Score</th>
+        <th>Right Score</th>
+        <th>Right Team</th>
+        <th>Winner</th>
+
+      </tr>
+      </thead>
+      <tbody>
+      {games.map((game, ind) =>
+        <ConfirmGameListRow key={game.id} game={game}
+                            users={users} currentUser={currentUser} num={ind+1} confirm={confirm} deny={deny}/>
+      )}
+      </tbody>
+    </table>
+  );
+};
+
+ConfirmGameList.propTypes = {
+  games: PropTypes.array.isRequired,
+  users: PropTypes.array.isRequired,
+  uid: PropTypes.string,
+  uid2: PropTypes.string,
+  uid3: PropTypes.string,
+  uid4: PropTypes.string,
+  uids: PropTypes.array,
+  league: PropTypes.string
+};
+
+export default ConfirmGameList;
---
+++ reactredux/src/components/admin/AdminGameListRow.js
@@ -0,0 +1,94 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import * as gameActions from '../../actions/gameActions';
+import * as userActions from '../../actions/userActions';
+import toastr from 'toastr';
+import {bindActionCreators} from 'redux';
+
+const ConfirmGameListRow = function({game, users, currentUser,confirm, deny, num}) {
+  console.log(this)
+  return (
+
+    <tr>
+      <td><Link to={"/confirm/"+(game.id)}>{(new Date (game.id)).toDateString()}</Link></td>
+      <td><Link to={"/profile/"+game.player_names.player_l_1}>{uidToNameLookup(game.player_names.player_l_1, users)} </Link><br />
+        <Link to={"/profile/"+game.player_names.player_l_2}>{uidToNameLookup(game.player_names.player_l_2, users)}</Link></td>
+
+      <td>{game.scores.left}</td>
+      <td>{game.scores.right}</td>
+      <td><Link to={"/profile/"+game.player_names.player_r_1}>{uidToNameLookup(game.player_names.player_r_1, users)}</Link><br />
+        <Link to={"/profile/"+game.player_names.player_r_2}>{uidToNameLookup(game.player_names.player_r_2, users)}</Link></td>
+
+      <td>{game.winner} Side</td>
+
+      <input
+        name={num.toString()}
+        type="submit"
+        value={'Confirm'}
+        className="btn btn-primary"
+        onClick={confirm}
+
+      />
+      &nbsp;
+      <input
+        name={num.toString()}
+        type="submit"
+        value={'Deny'}
+        className="btn btn-primary"
+        onClick={deny}
+
+      />
+
+    </tr>
+  );
+}.bind(this);
+
+ConfirmGameListRow.propTypes = {
+  game: PropTypes.object.isRequired,
+  users: PropTypes.array.isRequired
+};
+
+
+// one day inspect ownProps
+function mapStateToProps(state, ownProps){
+
+
+  const authorsFormattedForDropdown = state.users.map(author => {
+    return {
+      author
+
+    };
+  });
+
+  return {
+    authors: authorsFormattedForDropdown
+  };
+}
+
+function uidToNameLookup(uid, users){
+  //console.log(uid);
+  //console.log(users);
+  for(var x in users){
+    if (users[x].uid === uid){
+      return users[x].displayName;
+    }
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+
+  return {
+    actions: bindActionCreators(gameActions, dispatch),
+    useractions: bindActionCreators(userActions, dispatch)
+  };
+}
+function save(game, user) {
+
+  console.log(gameActions);
+  console.log(gameActions.saveGame(game))
+  userActions.saveUser(user);
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConfirmGameListRow);
